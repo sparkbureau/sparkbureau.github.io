00e806651bc14bc93059a44c9830457b5ad33f7d
sparkbureau <zach.johnson@mac.com>
2015-07-08 19:54:44 +1000

    v14
    adding newsletter signup form

--- js/agency.js
+++ js/agency.js
@@ -3,73 +3,70 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset()
+                .top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
+        target: '.navbar-fixed-top'
+    })
+    // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
 /******************************************************************************************************************************
 COMMING SOON PAGE
 *******************************************************************************************************************************/
 (function($) {
     /**
-    * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
-    * according to the GMT+0 Timezone
-    **/
+     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
+     * according to the GMT+0 Timezone
+     **/
     var launch = new Date(2015, 8, 1, 0, 00);
     /**
-    * The script
-    **/
+     * The script
+     **/
     var message = $('#message');
     var days = $('#days');
     var hours = $('#hours');
     var minutes = $('#minutes');
     var seconds = $('#seconds');
-    
     setDate();
-    function setDate(){
+
+    function setDate() {
         var now = new Date();
-        if( launch < now ){
+        if (launch < now) {
             days.html('<h1>0</H1><p>Day</p>');
             hours.html('<h1>0</h1><p>Hour</p>');
             minutes.html('<h1>0</h1><p>Minute</p>');
             seconds.html('<h1>0</h1><p>Second</p>');
             message.html('GRAND OPENING IN:');
-        }
-        else{
-            var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
-            var d = Math.floor(s/86400);
-            days.html('<h1>'+d+'</h1><p>Day'+(d>1?'s':''),'</p>');
-            s -= d*86400;
-
-            var h = Math.floor(s/3600);
-            hours.html('<h1>'+h+'</h1><p>Hour'+(h>1?'s':''),'</p>');
-            s -= h*3600;
-
-            var m = Math.floor(s/60);
-            minutes.html('<h1>'+m+'</h1><p>Minute'+(m>1?'s':''),'</p>');
-
-            s = Math.floor(s-m*60);
-            seconds.html('<h1>'+s+'</h1><p>Second'+(s>1?'s':''),'</p>');
+        } else {
+            var s = -now.getTimezoneOffset() * 60 + (launch.getTime() -
+                now.getTime()) / 1000;
+            var d = Math.floor(s / 86400);
+            days.html('<h1>' + d + '</h1><p>Day' + (d > 1 ? 's' : ''),
+                '</p>');
+            s -= d * 86400;
+            var h = Math.floor(s / 3600);
+            hours.html('<h1>' + h + '</h1><p>Hour' + (h > 1 ? 's' : ''),
+                '</p>');
+            s -= h * 3600;
+            var m = Math.floor(s / 60);
+            minutes.html('<h1>' + m + '</h1><p>Minute' + (m > 1 ? 's' :
+                ''), '</p>');
+            s = Math.floor(s - m * 60);
+            seconds.html('<h1>' + s + '</h1><p>Second' + (s > 1 ? 's' :
+                ''), '</p>');
             setTimeout(setDate, 1000);
-
             message.html('GRAND OPENING IN:');
         }
     }
