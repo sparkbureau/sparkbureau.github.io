947c5cbe29a33b2c2d764f1b291ec085f8d0b76b
sparkbureau <zach.johnson@mac.com>
2015-08-02 21:21:11 +1000

    Revert c43864c..7dfa9b1
    This rolls back to commit c43864cd7092c7fbd7576555164d35ebad6c9c61.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,5 @@
 <?php
-// check if fields passed are empty
+// Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
@@ -15,11 +15,11 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
-// create email body and send it	
-$to = 'info@sparkbureau.co'; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO*
+// Create the email and send the message
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: info@sparkbureau.co\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE TO BE FROM*
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
